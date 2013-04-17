2136da008c0800fcfa4a4fbec7e241a2d7c33b36
mario <mario@mario-desktop.(none)>
2013-04-17 07:06:45 -0400

    Agregado manejo del tiempo de ejecucion y guardado de resultados

--- src/ArrayGenerator.java
+++ src/ArrayGenerator.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -8,26 +11,34 @@ public class ArrayGenerator {
 	 * @param args
 	 */
 	
+	private static PrintWriter resultWriter;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-			for(int i=100000;i<=1000000;i=i+50000){
-			initArray(i);
-			}
-			
-			System.out.println("termine de generar inputs");
-			Scanner console = new Scanner(System.in);
-			System.out.print("You're next guess? ");
-			String guess = console.next();
-			/*
-			double val = Math.random()*1000;
-			int val1 =(int)val;
-			System.out.println(val);
-			System.out.println(Math.round(val));
-			
-			
+		resultWriter = new PrintWriter(new FileWriter("resultNanos.csv"));
+		resultWriter.println("#Resultados para algoritmo: ASDFLOLQWERTY");
+		
+		for(int i=100000;i<=1000000;i=i+50000){
+		initAndSort(i);
+		resultWriter.println("######");
+		}
+		
+		System.out.println("termine de generar inputs");
+		resultWriter.close();
 
-			*/
+		/*
+		Scanner console = new Scanner(System.in);
+		System.out.print("You're next guess? ");
+		String guess = console.next();
+		/*
+		double val = Math.random()*1000;
+		int val1 =(int)val;
+		System.out.println(val);
+		System.out.println(Math.round(val));
+		
+		
+
+		*/
 			
 			
 	
@@ -35,7 +46,7 @@ public class ArrayGenerator {
 	
 
 
-	private static void initArray(int length) {
+	private static void initAndSort(int length) {
 		// TODO Auto-generated method stub
 		int [][] input = new int[100][length];		
 		for(int i=0;i<100;i++){
@@ -43,9 +54,16 @@ public class ArrayGenerator {
 				input[i][k]=(int) Math.round(Math.random()*1000000);
 			}
 		}
+		
+		resultWriter.println("#Resultados para largo = "+length);
+		resultWriter.println("#Arreglo nº; tiempo");
+		long init=0, end = 0;
 		//Una vez generados, hacer algo con ellos:
 		for(int i=0;i<100;i++){
+			init=System.nanoTime();
 			Arrays.sort(input[i]);
+			end=System.nanoTime();
+			resultWriter.println(""+(i+1)+";"+(end-init));
 		}
 		System.out.println("Ordenados arreglos de largo: "+length);
 		/*
